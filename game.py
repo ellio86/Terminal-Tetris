48c4f6306c4d3a46ace1ecef463fc064d5420a5c
Elliot Kempson <kempsone@cardiff.ac.uk>
2021-11-28 02:17:40 +0000

    Working board with falling t block

--- game.py
+++ game.py
@@ -13,60 +13,52 @@ class Game_board:
         for line in self.board:
             string += str(line) + "\n"
         return string
-
+    
+    def add_block(self, x, y, z):
+        self.board[x][y] = z
+        
+    def check_block(self, x, y) -> int:
+        return self.board(x, y)
+    
 class Piece:
-    def __init__(self, shape: tuple) -> None:
-        self.is_new = True
-        self.shape = shape
-        self.starting = True
-        self.next_line_to_display = len(shape) - 1
-         
-def update_board(board, current_piece, player_x) -> list:
-    old_board = new_board = board
-    if current_piece.starting:
-        player_x = 5
-        current_piece.starting = False
-        
-    for i, board_line in enumerate(old_board[::-1]):
-        new_line = board_line
-        if i == len(old_board) - 1 and current_piece.is_new:
-            for n, pixel in enumerate(current_piece.shape[current_piece.next_line_to_display], start=player_x):
-                if not new_line[n] and pixel:
-                    new_line[n] = pixel
-                else:
-                    return None
-        
-            current_piece.next_line_to_display -= 1
-            if current_piece.next_line_to_display == - 1:
-                current_piece.is_new = False
-
-        for j, pixel in enumerate(board_line):
-            if pixel == 1:
-                if i < 0:
-                    below_line = old_board[i-1]
-                if below_line:
-                    if not below_line[j]:
-                        new_line[j] = 1
-                        
-        new_board.append(new_line)
-    return new_board[::-1], player_x
-
-                    
+    def __init__(self, position: list = [0, 5], blocks: list = None):
+        self.position = position
+        self.blocks = blocks if blocks else [(0, 0, 0), (0, 1, 1), (1, 0, 1), (1, 1, 1), (2, 1, 1)]
+        self.height = max([block[0] for block in self.blocks]) + 1
+    
+def draw_to_board(board: Game_board, *pieces: list[Piece]) -> None:
+    """Takes in a list of pieces and displays them on the board."""
+    for piece in pieces:
+        for block in piece.blocks:
+            x = piece.position[0] + block[0]
+            y = piece.position[1] + block[1]
+            board.add_block(x, y, block[2])  
+               
 def play_game():
     def _play_game(stdscr):
         stdscr.clear()
         board = Game_board(11, 21)
         playing = True
-        player_x = 5
+        active_piece = Piece(position=[-1, 5])
+        piece_speed = 1
         while playing:
+            time.sleep(0.5)
+            if active_piece.position[0] + 1 == board.height - active_piece.height:
+                piece_speed = 0 
+            
+            active_piece.position[0] += piece_speed
+            board.board[active_piece.position[0] - 1] = [n if n != 1 else 0 for n in board.board[active_piece.position[0] - 1]]
+            draw_to_board(board, active_piece)
+            
+            # Display the board
             for x, line in enumerate(board.board):
-                stdscr.addstr(x, 0, str(line))
-                stdscr.refresh()
+                for y, pixel in enumerate(line):
+                    stdscr.addstr(x, y, str(pixel))
+            stdscr.refresh()
             
             #code = stdscr.getch()
             #stdscr.refresh()
             #stdscr.timeout(100)
-            board.board, player_x = update_board(board.board, pieces[0], player_x)
             #match chr(code).lower():
                 #case ch:
                   #  board.board[0][0] = ch
@@ -75,8 +67,8 @@ def play_game():
 
     return curses.wrapper(_play_game)
 
-pieces = [Piece(([0, 0, 0, 0],
-                 [0, 1, 0, 0],
+pieces = [Piece(([0, 1, 0, 0],
                  [1, 1, 0, 0],
-                 [0, 1, 0, 0]))]
+                 [0, 1, 0, 0],
+                 [0, 0, 0, 0]))]
 print(play_game())
