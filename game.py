dfef807dc70ccbc24d1c4f2906f9bd03d4bf3916
Elliot Kempson <kempsone@cardiff.ac.uk>
2021-11-29 15:49:44 +0000

    Add other shapes, fixes #1

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-import time
+import time, random
 import curses
 from dataclasses import dataclass
 
@@ -20,11 +20,23 @@ class Game_board:
     def check_block(self, x, y) -> int:
         return self.board(x, y)
     
+    def clear(self) -> None:
+        """Removes any active blocks from the board."""
+        clear_board = []
+        for line in self.board:
+            newline = []
+            for space in line:
+                newline.append(0) if space == 1 else newline.append(space)
+                
+            clear_board.append(newline)
+        self.board = clear_board
+    
 class Piece:
-    def __init__(self, position: list = [0, 5], blocks: list = None):
+    def __init__(self, position: list = [0, 5], blocks: list = None, speed=1):
         self.position = position
-        self.blocks = blocks if blocks else [(0, 0, 0), (0, 1, 1), (1, 0, 1), (1, 1, 1), (2, 1, 1), (0, 2, 0)]
+        self.blocks = blocks if blocks else [(0, 1, 1), (1, 0, 1), (1, 1, 1), (2, 1, 1)]
         self.height = max([block[0] for block in self.blocks]) + 1
+        self.speed = speed
     
 def draw_to_board(board: Game_board, *pieces: list[Piece]) -> None:
     """Takes in a list of pieces and displays them on the board."""
@@ -39,15 +51,26 @@ def play_game():
         stdscr.clear()
         board = Game_board(11, 21)
         playing = True
-        active_piece = Piece(position=[-1, 5])
         piece_speed = 1
+        moved = False
+        pieces = [((0, 1, 1), (1, 0, 1), (1, 1, 1), (2, 1, 1)),
+                  ((0, 0, 1), (0, 1, 1), (1, 0, 1), (1, 1, 1)),
+                  ((0, 0, 1), (0, 1, 1), (1, 1, 1), (1, 2, 1))
+                 ]
+        active_piece = Piece(blocks=random.choice(pieces))
         while playing:
-            time.sleep(0.5)
             if active_piece.position[0] + 1 == board.height - active_piece.height:
-                piece_speed = 0 
+                active_piece = Piece(blocks=random.choice(pieces))
             
-            active_piece.position[0] += piece_speed
-            board.board[active_piece.position[0] - 1] = [n if n != 1 else 0 for n in board.board[active_piece.position[0] - 1]]
+            # Move every second
+            if str(time.time())[11] == "0":
+                if not moved:
+                    moved = True
+                    active_piece.position[0] += active_piece.speed
+                    
+            else:
+                moved = False
+                
             draw_to_board(board, active_piece)
             
             # Display the board
@@ -58,7 +81,8 @@ def play_game():
                             stdscr.addstr(x, y, chr(9633))
                         case 0:
                             stdscr.addstr(x, y, chr(9632))
-                        
+            
+            board.clear()
             stdscr.refresh()
             stdscr.timeout(100)
             
@@ -69,14 +93,12 @@ def play_game():
                         active_piece.position[1] -= 1
                     case "d":
                         active_piece.position[1] += 1
-                        
+                    case "s":
+                        active_piece.position[0] += 1
+                
                 stdscr.refresh()
                     
 
     return curses.wrapper(_play_game)
 
-pieces = [Piece(([0, 1, 0, 0],
-                 [1, 1, 0, 0],
-                 [0, 1, 0, 0],
-                 [0, 0, 0, 0]))]
 print(play_game())
